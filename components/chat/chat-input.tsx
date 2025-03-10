0082a5bd6c16672964f1d291b71acb40df4bf012
Aayush <aayushg2908@gmail.com>
2025-03-10 19:24:16 +0530

    feat: login before chatting with AI

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -4,11 +4,15 @@ import TextareaAutosize from "react-textarea-autosize";
 import { useState, KeyboardEvent, useRef, useEffect } from "react";
 import { Button } from "../ui/button";
 import { LoaderCircle, Paperclip, SendHorizontal } from "lucide-react";
+import { useLoginModal } from "@/hooks/use-login-modal";
+import { useSession } from "@/lib/auth-client";
 
 export const ChatInput = () => {
   const [input, setInput] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const inputRef = useRef<HTMLTextAreaElement>(null);
+  const { onOpen } = useLoginModal();
+  const { data } = useSession();
 
   useEffect(() => {
     inputRef.current?.focus();
@@ -19,8 +23,11 @@ export const ChatInput = () => {
     if (!message || isLoading) return;
 
     try {
+      if (!data?.user && !data?.user.id) {
+        onOpen();
+        return;
+      }
       setIsLoading(true);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
       console.log("Sending message:", message);
       setInput("");
       inputRef.current?.focus();
@@ -28,6 +35,19 @@ export const ChatInput = () => {
       console.error("Failed to send message:", error);
     } finally {
       setIsLoading(false);
+    }
+  };
+
+  const handleFileUpload = async () => {
+    console.log("handleFileUpload");
+    try {
+      if (!data?.user && !data?.user.id) {
+        onOpen();
+        return;
+      }
+      console.log("File uploaded");
+    } catch (error) {
+      console.error("Failed to upload file:", error);
     }
   };
 
@@ -63,6 +83,7 @@ export const ChatInput = () => {
               disabled={isLoading}
               size="icon"
               variant="ghost"
+              onClick={() => handleFileUpload()}
             >
               <Paperclip className="size-3.5" />
             </Button>
