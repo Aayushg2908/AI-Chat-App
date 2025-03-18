a306e9c38249e38a9c66422cc25c7a1910b4a273
Aayush <aayushg2908@gmail.com>
2025-03-18 15:33:24 +0530

    feat: model traits icons

--- components/chat/model-selector.tsx
+++ components/chat/model-selector.tsx
@@ -1,5 +1,12 @@
 import { Button } from "../ui/button";
-import { ChevronDown, Info } from "lucide-react";
+import {
+  ChevronDown,
+  Globe,
+  Info,
+  Zap,
+  FlaskConical,
+  Clock,
+} from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,27 +19,100 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "../ui/tooltip";
+import React from "react";
 
-const MODELS: { [key: string]: { id: string; description: string } } = {
+const MODELS: {
+  [key: string]: { id: string; description: string; icons: React.ReactNode[] };
+} = {
   "Gemini 1.5 Flash": {
     id: "gemini-1.5-flash-latest",
     description: "Google's old Flash model.",
+    icons: [
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger asChild>
+            <Clock key="clock" className="size-3 text-amber-400" />
+          </TooltipTrigger>
+          <TooltipContent className="bg-gray-200 text-black dark:bg-black dark:text-white text-xs">
+            Old Model
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>,
+    ],
   },
   "Gemini 1.5 Pro": {
     id: "gemini-1.5-pro-latest",
     description: "Google's old Pro model.",
+    icons: [
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger asChild>
+            <Clock key="clock" className="size-3 text-amber-400" />
+          </TooltipTrigger>
+          <TooltipContent className="bg-gray-200 text-black dark:bg-black dark:text-white text-xs">
+            Old Model
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>,
+    ],
   },
   "Gemini 2.0 Flash": {
     id: "gemini-2.0-flash-001",
     description: "Google's latest Flash model.",
+    icons: [
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger asChild>
+            <Globe key="globe" className="size-3 text-green-500" />
+          </TooltipTrigger>
+          <TooltipContent className="bg-gray-200 text-black dark:bg-black dark:text-white text-xs">
+            Web Search
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>,
+    ],
   },
   "Gemini 2.0 Pro": {
     id: "gemini-2.0-pro-exp-02-05",
-    description: "Google's latest Pro model.",
+    description: "Google's latest Experimental Pro model.",
+    icons: [
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger asChild>
+            <FlaskConical key="flask" className="size-3 text-red-400" />
+          </TooltipTrigger>
+          <TooltipContent className="bg-gray-200 text-black dark:bg-black dark:text-white text-xs">
+            Experimental
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>,
+    ],
   },
   "Gemini 2.0 Flash Lite": {
     id: "gemini-2.0-flash-lite-preview-02-05",
     description: "Google's Experimental and Faster model.",
+    icons: [
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger asChild>
+            <Zap key="flash" className="size-3 text-yellow-500" />
+          </TooltipTrigger>
+          <TooltipContent className="bg-gray-200 text-black dark:bg-black dark:text-white text-xs">
+            Very Fast
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>,
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger asChild>
+            <FlaskConical key="flask" className="size-3 text-red-400" />
+          </TooltipTrigger>
+          <TooltipContent className="bg-gray-200 text-black dark:bg-black dark:text-white text-xs">
+            Experimental
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>,
+    ],
   },
 };
 
@@ -52,6 +132,7 @@ const ModelSelector = ({
     "Gemini 1.5 Flash";
 
   const selectedModelDescription = MODELS[selectedModelName]?.description || "";
+  const selectedModelIcons = MODELS[selectedModelName]?.icons || [];
 
   return (
     <DropdownMenu>
@@ -62,12 +143,12 @@ const ModelSelector = ({
           className="flex items-center gap-1 text-xs dark:text-gray-400 text-gray-600 hover:dark:text-white hover:text-gray-950 transition-colors"
           disabled={disabled}
         >
-          {selectedModelName}
+          <span>{selectedModelName}</span>
           <ChevronDown className="size-3 ml-1" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="start">
-        {Object.entries(MODELS).map(([name, { id, description }]) => (
+        {Object.entries(MODELS).map(([name, { id, description, icons }]) => (
           <DropdownMenuItem
             key={id}
             onClick={() => setSelectedModel(id)}
@@ -75,17 +156,26 @@ const ModelSelector = ({
               selectedModel === id ? "bg-accent" : ""
             }`}
           >
-            <span>{name}</span>
-            <TooltipProvider>
-              <Tooltip delayDuration={0}>
-                <TooltipTrigger asChild>
-                  <Info className="size-3 ml-2 text-gray-400" />
-                </TooltipTrigger>
-                <TooltipContent className="bg-gray-200 text-black dark:bg-black dark:text-white text-xs">
-                  {description}
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
+            <div className="flex items-center">
+              <span>{name}</span>
+              <TooltipProvider>
+                <Tooltip delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <Info className="size-3 ml-1 text-gray-400" />
+                  </TooltipTrigger>
+                  <TooltipContent className="bg-gray-200 text-black dark:bg-black dark:text-white text-xs">
+                    {description}
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+            <div className="flex items-center space-x-1.5">
+              {icons.map((icon, index) => (
+                <React.Fragment key={`dropdown-icon-${id}-${index}`}>
+                  {icon}
+                </React.Fragment>
+              ))}
+            </div>
           </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
