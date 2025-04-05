a3c232a4a4417edd8fea847d7dfdc2c2761e2549
Aayush <aayushg2908@gmail.com>
2025-04-05 15:22:45 +0530

    feat: back to home and clone this thread button on shared thread page

--- app/share/[shareId]/shared-page.tsx
+++ app/share/[shareId]/shared-page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import ChatComponent from "@/components/chat";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { useLoginModal } from "@/hooks/use-login-modal";
+import { cn } from "@/lib/utils";
 import { Thread } from "@prisma/client";
 import { User } from "better-auth";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import { useEffect } from "react";
+import { toast } from "sonner";
 
 const SharedPage = ({
   thread,
@@ -25,7 +30,39 @@ const SharedPage = ({
 
   if (isOpen) return null;
 
-  return <ChatComponent thread={thread} isEditable={false} />;
+  const handleCloneThread = async () => {
+    try {
+      console.log("Cloned this Thread.");
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to clone this Thread.");
+    }
+  };
+
+  return (
+    <div className="w-full h-full overflow-hidden">
+      <header className="flex items-center justify-between mx-4 h-[50px]">
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({
+              variant: "ghost",
+            })
+          )}
+        >
+          <ArrowLeft className="size-4" />
+          Back to Home
+        </Link>
+        <Button
+          onClick={handleCloneThread}
+          className="bg-blue-600 text-white hover:bg-blue-700"
+        >
+          Clone this Thread
+        </Button>
+      </header>
+      <ChatComponent thread={thread} isEditable={false} />
+    </div>
+  );
 };
 
 export default SharedPage;
