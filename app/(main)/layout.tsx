414b905268c76949fb8991d57252b20d796453ac
Aayush <aayushg2908@gmail.com>
2025-05-05 13:28:43 +0530

    feat: only selected users can access the app

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,26 @@
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import SidebarTriggerComponent from "./sidebar-trigger";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-const MainLayout = ({ children }: { children: React.ReactNode }) => {
+const MainLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!process.env.APP_ACCESS?.includes(session?.user?.email!)) {
+    return (
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold">Access Denied</h1>
+        <p className="text-sm text-muted-foreground">
+          You do not have access to this application. Please contact the admin
+          for granting you access to this application.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <SidebarProvider>
       <AppSidebar />
