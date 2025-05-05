8303f35488ae56452c90fc2a243e1daff369bb13
Aayush <aayushg2908@gmail.com>
2025-05-05 13:18:02 +0530

    feat: deployed app url

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
 export const { signIn, signUp, useSession, signOut } = createAuthClient({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://all-in-one-nine-lemon.vercel.app",
 });
